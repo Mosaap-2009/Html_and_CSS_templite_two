e64ed80283c7d8c94d55ddc13c9ed12264362af8
Mosaap-2009 <mahmoudatyea1010@gmail.com>
2024-08-07 23:36:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+(1)
+
+
+let myfriends = ["Ahmed", "Sayed", "Ali", ["Shady", "Amr", ["Mohamed", "Gamal"]]]
+
+let [, , , [a, , [, b]]] = myfriends;
+
+console.log(a); // Shady
+console.log(b); // Gamal
+
+
+
+
+///.////////./////////.////////.//////././//////./////////
+
+
+ 
